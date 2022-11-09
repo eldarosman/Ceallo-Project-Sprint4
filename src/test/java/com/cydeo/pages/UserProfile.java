f1c62a6349dd03c6524d4214a8d2ee9b4791ac6a
umitenes <akdag.umit@ichc.ro>
2022-11-09 21:09:03 +0200

    CEA-1062_Profile

--- src/test/java/com/cydeo/pages/UserProfile.java
+++ src/test/java/com/cydeo/pages/UserProfile.java
@@ -1,4 +1,11 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class UserProfile {
+    public UserProfile(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
 }

6a42e5089972e65046df2f1a76a461fac8cb8037
Admin <bturchyk@gmail.com>
2022-11-10 15:55:03 +0200

    FeatureFile and StepDef are created

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = false,
-        tags = ""
+        dryRun = true,
+        tags = "@wip"
 )
 
 public class CukesRunner {
---
+++ src/test/java/com/cydeo/step_definitions/TalkModuleStepDefinition.java
@@ -0,0 +1,50 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TalkModuleStepDefinition {
+
+    @Given("user is on Talk page")
+    public void user_is_on_talk_page() {
+
+    }
+    @When("user create new group conversation")
+    public void user_create_new_group_conversation() {
+
+    }
+    @When("use add other users")
+    public void use_add_other_users() {
+
+    }
+    @Then("new group conversation is created")
+    public void new_group_conversation_is_created() {
+
+    }
+    @When("user click on conversation")
+    public void user_click_on_conversation() {
+
+    }
+    @Then("user can see the participants of the conversation")
+    public void user_can_see_the_participants_of_the_conversation() {
+
+    }
+    @Then("user can remove any participant from the conversation")
+    public void user_can_remove_any_participant_from_the_conversation() {
+
+    }
+    @When("user type a massage inside the inputbox")
+    public void user_type_a_massage_inside_the_inputbox() {
+
+    }
+    @Then("user can send this massage")
+    public void user_can_send_this_massage() {
+
+    }
+    @Then("user can delete this conversation")
+    public void user_can_delete_this_conversation() {
+
+    }
+
+}

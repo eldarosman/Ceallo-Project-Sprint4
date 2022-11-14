b85db34e3a69ecc487be1f1e7b043cd6730af723
Stanislav_Kovalov <109384979+Rowing2012@users.noreply.github.com>
2022-11-14 16:01:24 +0200

    Prepare DefStep

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = false,
-        tags = "@1"
+        dryRun = true,
+        tags = ""
 )
 
 public class CukesRunner {

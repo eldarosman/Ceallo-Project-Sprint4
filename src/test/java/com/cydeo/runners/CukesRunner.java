3514d6f744b45020ef133903a757b2bd567d4f12
Oleksandra Medvedieva <sashazibt940@gmail.com>
2022-11-16 17:13:22 +0200

    add plugin to CukesRunner

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,11 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
         plugin = {
                 "json:target/cucumber.json",
-                "html:target/cucumber-reports.html",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-        },
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",},
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,

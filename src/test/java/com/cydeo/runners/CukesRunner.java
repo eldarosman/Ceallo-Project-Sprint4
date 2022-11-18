0c8f9d65ada10a60fb9f8ac54e1d5b65cee7007a
Tabita465 <tabitapohus12@gmail.com>
2022-11-18 21:34:28 +0200

    commit

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -16,6 +16,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
+        tags = "@smoke"
+
 
 )
 
